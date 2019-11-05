aa55c5793eea2b81b6d962c1740553727b841999
Nirvach <Nirvach@github.com>
2019-11-06 00:00:16 +0300

    Initial commit

---
+++ src/OddNumber.java
@@ -0,0 +1,22 @@
+public class OddNumber {
+
+    public static void main(String... args) {
+
+        int result = 0;
+        int[] array = new int[10];
+
+        System.out.print("Array: ");
+        for ( int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 200 + 1);
+            System.out.print(array[i] + " ");
+        }
+
+        for (int j : array) {
+            if ((j % 2 != 0) && (j > result)) {
+                result = j;
+            }
+        }
+        
+        System.out.print("\nBiggest odd number - " + result);
+    }
+}
